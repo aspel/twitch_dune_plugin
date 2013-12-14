20e27dd1cbb361118d80c090b6cdf4ea4818eedb
aspel <lip@lip.net.ua>
2013-12-14 17:42:52 +0200

    Test6

--- channels_menu.php
+++ channels_menu.php
@@ -2,16 +2,13 @@
 	require_once "base_menu.php";
 	require_once "tw_database.php";
 	
-	class DateMenu extends BaseMenu
-	{
-		public function generate_menu()
-		{
+	class DateMenu extends BaseMenu {
+		public function generate_menu() {
 			$database = new TwDatabase();
 			$dates = $database->getDates();
 			rsort($dates);
 			
-			foreach ($dates as $date)
-			{
+			foreach ($dates as $date) {
 				$items[] = array("caption" => strftime("%Y-%m-%d", $date), "url" => "date:".$date);
 			}
 			
