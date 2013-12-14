342ad6a511fd7f767d0ad9f04122abe257b51d50
aspel <lip@lip.net.ua>
2013-12-14 17:23:01 +0200

    Test2

--- main_plugin.php
+++ main_plugin.php
@@ -1,78 +1,64 @@
 <?php
+    
     require "main_menu.php";
     require "games_menu.php";
     require "games_search_menu.php";
     require "channels_menu.php";
     require "game_play.php"
 	
-	class TwitchPlugin implements DunePlugin
-	{
-		public function get_folder_view($media_url, &$plugin_cookies)
-		{
-           	if (strpos($media_url, "games_search:") === 0)
-            {
+	class TwitchPlugin implements DunePlugin {
+		
+        public function get_folder_view($media_url, &$plugin_cookies) {
+
+           	if (strpos($media_url, "games_search:") === 0) {
                 $menu = new GameSearchMenu(substr($media_url, 13));
             }
-			else if ($media_url == "games")
-			{
+			else if ($media_url == "games") {
 				$menu = new GamesMenu();
 			}
-			else if ($media_url == "channels")
-			{
+			else if ($media_url == "channels") {
 				$menu = new ChannelsMenu();
 			}
-			else
-			{
+			else {
 				$menu = new MainMenu();
 			}
 			
 			return $menu->generate_menu();
 		}
 
-		public function get_next_folder_view($media_url, &$plugin_cookies)
-		{
+		public function get_next_folder_view($media_url, &$plugin_cookies) {
 		}
 
-		public function get_tv_info($media_url, &$plugin_cookies)
-		{
+		public function get_tv_info($media_url, &$plugin_cookies) {
 		}
 
-		public function get_tv_stream_url($media_url, &$plugin_cookies)
-		{
+		public function get_tv_stream_url($media_url, &$plugin_cookies) {
 		}
 
-		public function get_vod_info($media_url, &$plugin_cookies)
-		{
+		public function get_vod_info($media_url, &$plugin_cookies) {
 
-            if (strpos($media_url, "stream_name:") === 0)
-            {
+            if (strpos($media_url, "stream_name:") === 0) {
                 $stream = new GamePlay(substr($media_url, 12));
                 return $stream->generatePlayInfo();
             }
 		}
 
-		public function get_vod_stream_url($media_url, &$plugin_cookies)
-		{
+		public function get_vod_stream_url($media_url, &$plugin_cookies) {
 		}
 
-		public function get_regular_folder_items($media_url, $from_ndx, &$plugin_cookies)
-		{
+		public function get_regular_folder_items($media_url, $from_ndx, &$plugin_cookies) {
 		}
 
-		public function get_day_epg($channel_id, $day_start_tm_sec, &$plugin_cookies)
-		{
+		public function get_day_epg($channel_id, $day_start_tm_sec, &$plugin_cookies) {
 		}
 
-		public function get_tv_playback_url($channel_id, $archive_tm_sec, $protect_code, &$plugin_cookies)
-		{
+		public function get_tv_playback_url($channel_id, $archive_tm_sec, $protect_code, &$plugin_cookies) {
 		}
 
-		public function change_tv_favorites($op_type, $channel_id, &$plugin_cookies)
-		{
+		public function change_tv_favorites($op_type, $channel_id, &$plugin_cookies) {
 		}
 
-		public function handle_user_input(&$user_input, &$plugin_cookies)
-		{
+		public function handle_user_input(&$user_input, &$plugin_cookies) {
 		}
 	}
 ?>
