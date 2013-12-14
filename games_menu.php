3770b95102ba2f063a7583e05edaafee97016468
aspel <lip@lip.net.ua>
2013-12-14 17:29:47 +0200

    Test5

--- games_menu.php
+++ games_menu.php
@@ -1,22 +1,19 @@
 <?php
+
 	require_once "base_menu.php";
 	require_once "tw_database.php";
 	
-	class GamesMenu extends BaseMenu
-	{
-		public function generate_menu()
-		{
+	class GamesMenu extends BaseMenu {
+		public function generate_menu() {
 			$database = new TwDatabase();
 			
 			$items = array();
-			foreach ($database->database as $game)
-			{
+			foreach ($database->database as $game) {
 				$items[] = array("caption" => $game->name, "url" => "games_search:".$game->name);
-
 			}
 			usort($items, array("BaseMenu", "CompareCaption"));
 			
 			return $this->create_folder_view($items);
-		}
-	}
+        }
+    }
 ?>
