fd477afde282b308a8ea704e4ad49e7598c66901
aspel <lip@lip.net.ua>
2013-12-14 19:09:41 +0200

    Test7

--- games_search_menu.php
+++ games_search_menu.php
@@ -1,27 +1,24 @@
 <?php
+
 	require_once "base_menu.php";
 	require_once "tw_database.php";
 	
-	class GameSearchMenu extends BaseMenu
-	{
+	class GameSearchMenu extends BaseMenu {
         private $date;
 
-        function __construct($name)
-        {
+        function __construct($name) {
             $this->name = $name;
         }
-		public function generate_menu()
-		{
+		public function generate_menu() {
 			$database = new Tw_Search_stream($this->name);
 			
 			$items = array();
 
-			foreach ($database->database as $game)
-			{
+			foreach ($database->database as $game) {
 				$items[] = array("caption" => $game->display_name, "url" => "stream_name:".$game->name);
-
 			}
 			usort($items, array("BaseMenu", "CompareCaption"));
+            
             $this->iconFile = "gui_skin://small_icons/video_file.aai";
             $this->action = PLUGIN_VOD_PLAY_ACTION_ID;
 			
