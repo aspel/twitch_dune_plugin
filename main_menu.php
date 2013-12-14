5676a8411fce6cbcbeddcc3cd7b5bf036ec4adf3
aspel <lip@lip.net.ua>
2013-12-14 17:18:50 +0200

    Testing

--- main_menu.php
+++ main_menu.php
@@ -1,14 +1,13 @@
 <?php
-	require_once "base_menu.php";
+    
+    require_once "base_menu.php";
 	
-	class MainMenu extends BaseMenu
-	{
-		public function generate_menu()
-		{
-			$menu_items = array(
-							array("caption" => "Games", "url" => "games"),
-							array("caption" => "Channels", "url" => "channels"),
-						);
+    class MainMenu extends BaseMenu {
+	    public function generate_menu() {
+		    $menu_items = array(
+			    array("caption" => "Games", "url" => "games"),
+				array("caption" => "Channels", "url" => "channels"),
+				);
 			
 			return $this->create_folder_view($menu_items);
 		}
