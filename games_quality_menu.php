001a0195df0e1f6efc531e72c66a7d263577e9f0
aspel <lip@lip.net.ua>
2013-12-15 13:29:58 +0200

    Fix

--- games_quality_menu.php
+++ games_quality_menu.php
@@ -17,6 +17,7 @@
             $i=0;
             $matches = $database->database;
 			foreach ($matches as $game) {
+                hd_print("-->>".$game[2]);
                 $items[] = array("caption" => $game[2], "url" => "stream_name:".$game[3][0]);
                 $i++;
             }   
