ec113dc0123fb18cc9a9d4a52b7cf0b4bda791a9
aspel <lip@lip.net.ua>
2013-12-15 09:43:40 +0200

    Fix

--- game_play.php
+++ game_play.php
@@ -15,9 +15,9 @@ class GamePlay {
         $ts = "token=".urlencode($tokens->token)."&sig=".urlencode($tokens->sig);
         $m3u8_url = "http://usher.twitch.tv/api/channel/hls/".$this->streamName.".m3u8?".$ts;
         $hls_data = HD::http_get_document($m3u8_url);
-        preg_match('/^http:.*/', $hls_data, $matches);
+        preg_match('/http:.*/', $hls_data, $matches);
+        hd_print ($matches);
         $this->streamURL = $matches[0];
-        hd_print ($this->streamURL);
 
     }
                 
